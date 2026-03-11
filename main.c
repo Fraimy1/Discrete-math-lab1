26ad88a1c06469fc8dd545ab366972e0de3afdc0
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2026-03-11 08:24:49 +0300

    addition

--- main.c
+++ main.c
@@ -33,17 +33,65 @@ void bigint_from_int(BigInt *num, int int_num)
     num->digits[0] = 1;
     num->msd = int_num;
 }
+unsigned int min(unsigned int a, unsigned int b)
+{
+    if (a >= b)
+        return b;
+    else
+        return a;
+}
+
+unsigned int max(unsigned int a, unsigned int b)
+{
+    if (a >= b)
+        return a;
+    else
+        return b;
+}
 
 void bigint_add(BigInt* a, BigInt* b)
 {
-    unsigned int smaller_length = a->digits[0] > b->digits[0] ? b->digits[0] : a->digits[0];
+    unsigned int min_len = min(a->digits[0], b->digits[0]);
+    unsigned int max_len = max(a->digits[0], b->digits[0]);
     
-    
-    for (size_t i = 0; i < smaller_length-1; i++)
-    {
-        if ()
+    unsigned int a_is_max_len = max_len == a->digits[0];
+
+    unsigned int *tmp = realloc(a->digits, sizeof(unsigned int) * (max_len + 2));
+    if (tmp == NULL) return;
+    a->digits = tmp;
+
+    unsigned int carry = 0;
+    unsigned long long digit_sum = 0;
+
+    for (size_t i = 1; i <= max_len; i++)
+    {   
+        if (i <= min_len)
         {
-            /* code */
+            digit_sum = a->digits[i] + b->digits[i] + carry;
+            carry = 0;
+            if (digit_sum >= BASE)
+            {
+                digit_sum %= BASE;
+                carry = 1;
+            }
+            a->digits[i] = digit_sum;
+        }else
+        {
+            digit_sum = a->digits[i] + b->digits[i] + carry;
+            carry = 0;
+            
+            if (digit_sum >= BASE)
+            {
+                digit_sum %= BASE;
+                carry = 1;
+            }
+
+            if (!a_is_max_len) // a len < b len
+            {
+                realloc(a->digits, sizeof(unsigned int)*(a->digits[0]+2));
+            }
+            a->digits[i] = digit_sum;
+            
         }
         
     }
